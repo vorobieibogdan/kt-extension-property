ab9da3b049cb2a5a246bd09801085ca8cb706240
Bohdan Vorobiei <vbs@ukr.net>
2025-12-15 17:26:54 +0200

    task completed5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
+package mate.academy
+
 private const val ADULT_AGE = 18
+
+// Please, do not modify this class!
+class Person(val name: String, val age: Int)
 
 val Person.isAdult: Boolean
     get() = age >= ADULT_AGE
 
-// Please, do not modify this class!
-class Person(val name: String, val age: Int)
