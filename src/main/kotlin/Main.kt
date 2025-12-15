260ed634fefae2dccc7fc38718000aca46dde905
Bohdan Vorobiei <vbs@ukr.net>
2025-12-15 17:19:33 +0200

    task completed2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package mate.academy
 
+import Person
+
 val Person.isAdult: Boolean
     get() = age >= 18
-
--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,6 +1,1 @@
-// Please, do not modify this class!
-// Imagine, this class is provided by an external library,
-// and you don't have access to its source code.
-// Provide your solution in the Main.kt class
-
 class Person(val name: String, val age: Int)
